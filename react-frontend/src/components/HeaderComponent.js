d8ac90b6f83dc4ae28701fd9d701a5213585e8fb
CelloxHolmes <d.nutdanai.n@gmail.com>
2024-02-21 04:15:54 +0700

    fix logout button not show in login page

--- react-frontend/src/components/HeaderComponent.js
+++ react-frontend/src/components/HeaderComponent.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { withRouter, Link } from 'react-router-dom';
 
 class HeaderComponent extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-                 
-        }
-    }
+    handleLogout = () => {
+        localStorage.removeItem('USER_KEY'); // Adjust if your token storage key differs
+        this.props.history.push('/login');
+    };
 
     render() {
+        const isLoggedIn = !!localStorage.getItem('USER_KEY');
+        const isLoginPage = this.props.location.pathname === '/login';
+
         return (
             <div>
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a href="https://javaguides.net" className="navbar-brand">Employee Management App</a></div>
+                        <Link to="/" className="navbar-brand">Employee Management App</Link>
+                        {isLoggedIn && !isLoginPage && ( // Only show logout button if logged in and not on login page
+                            <div className="ml-auto">
+                                <button onClick={this.handleLogout} className="btn btn-info">Logout</button>
+                            </div>
+                        )}
                     </nav>
                 </header>
             </div>
-        )
+        );
     }
 }
 
-export default HeaderComponent
+export default withRouter(HeaderComponent);
